234eb43935face8cc22f7a1c567762fffb37387c
Victoria_Cardwell <victoria.cardwell1@gmail.com>
2020-12-08 11:35:48 -0500

    show, update and delete routes added

--- resources/songs.py
+++ resources/songs.py
@@ -9,6 +9,8 @@ from playhouse.shortcuts import model_to_dict
 # second argument is it's import_name
 song = Blueprint('songs', 'song')
 
+# INDEX ROUTE
+
 @song.route('/', methods=["GET"])
 def get_all_songs():
     ## find the songs and change each one to a dictionary into a new array
@@ -17,6 +19,17 @@ def get_all_songs():
         return jsonify(data=songs, status={"code": 200, "message": "Success"})
     except models.DoesNotExist:
         return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"})
+
+# SHOW ROUTE 
+
+@song.route('/<id>', methods=["GET"])
+def get_one_song(id):
+    print(id, 'reserved word?')
+    song = models.Song.get_by_id(id)
+    print(song.__dict__)
+    return jsonify(data=model_to_dict(song), status={"code": 200, "message": "Success"})
+
+# POST ROUTE
 
 @song.route('/', methods=["POST"])
 def create_songs():
@@ -28,3 +41,20 @@ def create_songs():
     # Change the model to a dict
     song_dict = model_to_dict(song)
     return jsonify(data=song_dict, status={"code": 201, "message": "Success"})
+
+# UPDATE ROUTE
+
+@song.route('/<id>', methods=["PUT"])
+def update_song(id):
+    payload = request.get_json()
+    query = models.Song.update(**payload).where(models.Song.id==id)
+    query.execute()
+    return jsonify(data=model_to_dict(models.Song.get_by_id(id)), status={"code": 200, "message": "resource updated successfully"})
+
+# DELETE ROUTE
+
+@song.route('/<id>', methods=["Delete"])
+def delete_song(id):
+    query = models.Song.delete().where(models.Song.id==id)
+    query.execute()
+    return jsonify(data='resource successfully deleted', status={"code": 200, "message": "resource deleted successfully"})
